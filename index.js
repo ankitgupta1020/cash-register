849da142341a501386cd79b86a44342ba12b2a16
ANKIT GUPTA <ankitgupta1181@gmail.com>
2022-08-15 19:28:48 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    // message.style.display = "none";
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
